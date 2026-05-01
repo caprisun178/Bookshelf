69f566555009cba78b61745af83d50454c510182
Sarah Parisi <siparisi@uncg.edu>
2026-05-01 17:50:38 -0400

    Updated app theming

--- bookshelf/client/src/App.jsx
+++ bookshelf/client/src/App.jsx
@@ -1,21 +1,36 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import BookSearchPage from "./pages/BookSearchPage";
 import BookshelfPage from "./pages/BookshelfPage";
 import BookDetailsPage from "./pages/BookDetailsPage";
+import logo from "./assets/bookshelf.png";
+import "./App.css";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <Link to="/">Bookshelf</Link>
-        <Link to="/search">Search</Link>
-      </nav>
+      <div className="app">
+        <header className="app-header">
+          <img src={logo} alt="Bookshelf logo" className="header-logo" />
 
-      <Routes>
-        <Route path="/" element={<BookshelfPage />} />
-        <Route path="/search" element={<BookSearchPage />} />
-        <Route path="/books/:workId" element={<BookDetailsPage />} />
-      </Routes>
+          <nav className="header-nav">
+            <NavLink to="/" className="nav-link">
+              Bookshelf
+            </NavLink>
+
+            <NavLink to="/search" className="nav-link">
+              Search
+            </NavLink>
+          </nav>
+        </header>
+
+        <main className="app-main">
+          <Routes>
+            <Route path="/" element={<BookshelfPage />} />
+            <Route path="/search" element={<BookSearchPage />} />
+            <Route path="/books/:workId" element={<BookDetailsPage />} />
+          </Routes>
+        </main>
+      </div>
     </BrowserRouter>
   );
 }
